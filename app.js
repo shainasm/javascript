1fe782294dbae1a2c0940a1c771899df773963b0
shainasm <smulani2002@google.com>
2021-10-06 06:43:18 -0700

    jss

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-// let response = fetch('http://jsonplaceholder.typicode.com/users').then(res => res.json())
-
-window.prompt("hii")
-// used to display a message on the console which sys hii and takes an input 
+let name= new Promise((resolve,reject) => {
+  setTimeout(() => {
+    resolve("hii shaina");
+  },3000);
+});

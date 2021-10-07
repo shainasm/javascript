e2e841165ec61577d347f3759cfff8185fd4419a
shainasm <smulani2002@google.com>
2021-10-07 02:12:29 -0700

    primitive datatypes

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+//strings
 console.log("hello world")
-console.warn("hello world")
-console.error("hello world")
+console.warn('hello world')
+
+// consle is operator which is used to display on the console screen
+
+//numbers
+console.log(typeof "hello world")
+console.log(typeof 'hello world')
+console.log(typeof 1)
+console.log(typeof -1)
+console.log(typeof 1.4)
+
+// boolean
+console.log(typeof true)
+console.log(typeof false)

9921e4bdf51f525311f48b11fc11a29cf14b690c
shainasm <smulani2002@google.com>
2021-10-07 02:25:21 -0700

    null and undefined

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-//strings
-console.log("hello world")
-console.warn('hello world')
-console.error('hello world')
+var age = null
 
+// console.log(Name)
 
-// consle is operator which is used to display on the console screen
-
-//numbers
-console.log(typeof "hello world")
-console.log(typeof 'hello world')
-console.log(typeof 1)
-console.log(typeof -1)
-console.log(typeof 1.4)
-
-// boolean
-console.log(typeof true)
-console.log(typeof false)
+console.log(typeof age)

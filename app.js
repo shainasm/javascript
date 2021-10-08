36109d4ebb654b737bd8b1f5f36f5b6273ef1a16
shainasm <smulani2002@google.com>
2021-10-07 22:00:17 -0700

    difference betn get elements by class name and query selector

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var heading = document.getElementById('heading')
 var para = document.getElementsByClassName('para')
+console.log( 'by get method',para)
+
+// get elements by class name method gives the data in the form of an array
+
+// an query selector gives the particular element individually
 
 // query selector is used to select an element. it accepts a css selector....eg to select an element of id para:- var query =document.queryselctor('#para')
 
-var tags = document.querySelector('a')
+
+var para = document.querySelector('.para')
+console.log('by query selector;', para)
+
 var query = document.querySelector('h1')
 
 console.log(query)

a21382caccb6ec59b76e66a89497ec46f2a88b37
shainasm <smulani2002@google.com>
2021-10-07 22:03:58 -0700

    query selctorall

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 var heading = document.getElementById('heading')
 var para = document.getElementsByClassName('para')
-console.log( 'by get method',para)
+var para = document.getElementsByTagName('a')
+var query = document.querySelector('.para')
+var query = document.querySelectorAll('.para')
 
-// get elements by class name method gives the data in the form of an array
-
-// an query selector gives the particular element individually
-
-// query selector is used to select an element. it accepts a css selector....eg to select an element of id para:- var query =document.queryselctor('#para')
-
-
-var para = document.querySelector('.para')
-console.log('by query selector;', para)
-
-var query = document.querySelector('h1')
 
 console.log(query)

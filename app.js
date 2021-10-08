c16f048ed8743b3a2d2d6a9c051fa24971c389e4
shainasm <smulani2002@google.com>
2021-10-07 22:26:12 -0700

    classlist add, remove and dark mode

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var heading = document.getElementById('heading')
-var para = document.getElementsByClassName('para')
-var para = document.getElementsByTagName('a')
-var query = document.querySelector('.para')
-var query = document.querySelectorAll('.para')
+var body = document.querySelector('body')
 
+heading.innerHTML = "I was added by js"
+heading.style = "color:red;font-size:2rem"
 
-console.log(query)
+heading.classList.add('five')
+heading.classList.remove('one')
+
+body.classList.add('dark')
+
+// .add to add a class to an element
+// .remove to remove a class from an element
+// classlist shows the list of classes the element  has.

ac3f8570d69f4114a4a8bbe2f27f29b72f9f6c6e
shainasm <smulani2002@google.com>
2021-10-07 21:41:20 -0700

    DOM API selection methods

--- app.js
+++ app.js
@@ -1,58 +1,9 @@
-//operators
+var heading = document.getElementById('heading')
+var para = document.getElementsByClassName('para')
 
-// arithmetic operators
+// query selector is used to select an element. it accepts a css selector....eg to select an element of id para:- var query =document.queryselctor('#para')
 
-var a = 2
-var b = 10
+var tags = document.querySelector('a')
+var query = document.querySelector('h1')
 
-console.log('a+b=', a+b)
-console.log('b-a=', b-a)
-console.log('a*b=', a*b)
-console.log('a**b=', a**b)
-console.log('a/b=', a/b)
-console.log('a%b=', a%b)
-
-// assignment operators
-
-console.log('b', b)
-b += 10
-console.log('b+=10', b)
-b++
-console.log('b++', b)
-b-=10
-console.log('b-=10', b)
-b--
-console.log('b--', b)
-
-var c = 10
-console.warn('c++', c++)
-console.warn('++c', ++c)
-
-// comparison operator
-
-var d = 20
-var e = 22
-var f = '20'
-
-console.log(d<e)
-console.log(d>e)
-console.log(d==e)
-console.log(d<=e)
-console.log(d>=e)
-console.log(d==f)
-console.log(d!=f)
-console.log(d===f)
-console.warn(d+f)
-
-// logical operators
-
-console.log('true&&true', true&&true)
-console.log('true&&false', true&&false)
-console.log('false&&true',false&&true)
-console.log('false&&false', false&&false)
-
-console.log('true||true', true||true)
-console.log('true||false', true||false)
-console.log('false||true', false||true)
-console.log('false||false',false||false)
-
+console.log(query)

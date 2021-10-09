52d505f37460a4a4e6c7d838e3c5cbc717ce72d8
shainasm <smulani2002@google.com>
2021-10-09 05:37:01 -0700

    arrow function

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// // function is a block of code which performs a specific function. parameters are the values which may or may not be passed to a function
-
 // function greet(Name='john',age=99) {
 //   console.log(Name, age)
 //   // console.log('hey '+ Name + '!! your age is '+ age)
@@ -18,17 +16,11 @@
 // }
 
 // function expression:- (most prefferable)
-var add = function(a=0 , b=0){
+var add = (a=0 , b=0) => {
   var sum = a + b
   return sum
 }
 
 console.log(add(10,20))
 
-// example for practise
-// var func = function(Name='john', age=99){
-//   var greet = Name + age
-//   return greet
-// }
 
-// console.log(func('shaina ', 19))

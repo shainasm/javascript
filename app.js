2063645a8a0ada4c02371bf66556542904dca309
shainasm <smulani2002@google.com>
2021-10-10 02:48:48 -0700

    dark mode of the todo list

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-// var arr=['go to the gym','cook oatmeal','eat']
-
-// arr.push('sleep')
-// // for(var i=0; i < arr.length; i++) {
-// //   console.log(arr[i])
-// // }
-
-// // var callbackFunction = (element , index) => {
-// //   console.log(element , index)
-// // }
-
-// // another way of callback function
-// arr.forEach((element , index) => {
-//   console.log(element , index)
-// })
-
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
+var body = document.querySelector('body')
 
 const callbackFunc= (event) => {
-  // console.log(input.value)
-  const inputValue = input.value
-  const element = document.createElement('li')
-  const textNode = document.createTextNode(inputValue)
-  element.appendChild(textNode)
-  list.appendChild(element)
+ body.classList.toggle('dark')
 }
 
 button.addEventListener('click',callbackFunc)

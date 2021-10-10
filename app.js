9bf7eb3b1aca05c6c86b03ddfce18277df15b5d9
shainasm <smulani2002@google.com>
2021-10-10 06:28:38 -0700

    array methods: shift, unshift, splice, slice and filter

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
-// selection of elements from the html document
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-const add = document.querySelector('#add')
-const subtract = document.querySelector('#subtract')
-const multiply = document.querySelector('#multiply')
-const divide = document.querySelector('#divide')
-const resultBox = document.querySelector('.result')
+// unshift add the element to the beggining of the array
+// push adds the element to the end of the array
+// shift removes the particular element of the array
+// slice cuts the particular range from the array and adds them to a new array with no change in the original array
+// splice deletes the particular items from the array
 
-// // function add 
-// const sum = () => {
-//   const result = parseInt(a.value) + parseInt(b.value)
-//   // parseit converts a string into an integer
-//   console.log(result)
-//   resultBox.innerHTML = result
+var arr=[1, 2, 3, 4, 5, 6, 7, 8]
+
+var new_arr = arr.filter((element,index) => {
+  if(element % 2 === 0) return true
+})
+
+// for(var i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) new_arr.push(arr[i])
 // }
 
-// // function multiply 
-// const product = () => {
-//   const result = parseInt(a.value) * parseInt(b.value)
-//   console.log(result)
-//   resultBox.innerHTML = result
-// }
-
-const calculate = (event, operation) => {
-  // console.log(event.target)
-  // if (operation=='add') {
-  //   console.log('adding')
-  // } else if (operation=='multiply') {
-  //   console.log('multiplying')
-  // }
-
-  switch(operation) {
-    case "add":
-      resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
-      break
-    case "subtract":
-      resultBox.innerHTML = parseInt(a.value) - parseInt(b.value)
-      break
-    case "multiply":
-      resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)
-      break
-    case "divide":
-      resultBox.innerHTML = parseInt(a.value) / parseInt(b.value)
-      break
-    default:
-      resultBox.innerHTML = "Not a valid operation"
-  }
-}
-
-add.addEventListener('click', (event) => calculate(event,'add'))
-
-subtract.addEventListener('click', (event) => calculate(event,'subtract'))
-
-multiply.addEventListener('click', (event) => calculate(event,'multiply'))
-
-divide.addEventListener('click', (event) => calculate(event,'divide'))
+console.log(new_arr)
